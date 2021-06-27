0348db6ab4322252a18dc0259b998e18b7a34ee9
Erik Johnson <erik.k.johnson@colorado.edu>
2021-06-27 00:21:53 -0600

    added

--- MutualInfo/setup.py
+++ MutualInfo/setup.py
@@ -2,26 +2,22 @@ import io
 from pkgutil import walk_packages
 from setuptools import setup
 
-
 def find_packages(path):
     # This method returns packages and subpackages as well.
     return [name for _, name, is_pkg in walk_packages([path]) if is_pkg]
-
 
 def read_file(filename):
     with io.open(filename) as fp:
         return fp.read().strip()
 
-
 def read_requirements(filename):
     return [line.strip() for line in read_file(filename).splitlines()
             if not line.startswith('#')]
 
-
 setup(
-    name="",
+    name="MutualInfo",
     packages=list(find_packages('.')),
-    version="",
+    version="0.0.1",
     author="Erik Johnson",
     author_email="erik.k.johnson@colorado.com",
     description="",
